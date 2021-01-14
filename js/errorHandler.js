931f58edaa6f9e6c1e530c4e605b814675d72327
ayxan386 <ayxanpro@gmail.com>
2021-01-14 20:51:34 +0400

    Fixed error messages

--- js/errorHandler.js
+++ js/errorHandler.js
@@ -2,6 +2,7 @@ $(document).ajaxError(function (event, jqxhr, settings, thrownError) {
   if (jqxhr.status == 0) {
     window.location.replace("login.html");
   } else {
-    alert(jqxhr.reponseJSON ? jqxhr.reponseJSON.message : "Unknown error");
+    console.error(jqxhr);
+    alert(jqxhr.responseJSON ? jqxhr.responseJSON.message : "Unknown error");
   }
 });
